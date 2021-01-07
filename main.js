1d60632b2e55aeccfa7f54f392e896187cf48b0a
gael-src <src.gael@gmail.com>
2021-01-07 08:32:18 +0100

    started canvas

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("John Horton Conway");
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+// SIZE
+const size = 800;
+// SCALE
+const scale = 8;
+
+setup();
+
+function setup() {
+	// SIZE
+	canvas.width = size;
+	canvas.height = size;
+	// SCALE
+	context.scale(scale, scale);
+	// RECTANGLE FILL
+	context.fillStyle = "black";
+	context.fillRect(50, 50, 1, 1);
+}
