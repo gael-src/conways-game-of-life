c461bdc618131debe345bcb32a1d8137a975b5d7
gael-src <src.gael@gmail.com>
2021-01-07 09:17:00 +0100

    I need food

--- main.js
+++ main.js
@@ -1,34 +1,55 @@
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
-// SIZE
-const size = 800;
-// SCALE
-const scale = 8;
-// RESOLUTION
-const resolution = size / scale;
+const size = 800; // SIZE
+const scale = 8; // SCALE
+const resolution = size / scale; // RESOLUTION
 
-// SETUP CALL
-setup();
-// RANDOMCELSS CALL
-randomCells();
+let cells; // CELLS
+
+setup(); // SETUP CALL
+// createCells(); // CREATE CELLS CALL
+randomCells(); // RANDOM CELLS CALL
+drawCells(); // DRAW CELLS CALL
 
 // SETUP FUNCTION
 function setup() {
-	// SIZE
-	canvas.width = size;
-	canvas.height = size;
-	// SCALE
-	context.scale(scale, scale);
-	// RECTANGLE FILL
-	context.fillStyle = "black";
-	context.fillRect(50, 50, 1, 1);
+	canvas.width = size; // SIZE
+	canvas.height = size; // SIZE
+	context.scale(scale, scale); // SCALE
+	context.fillStyle = "black"; // RECTANGLE FILL
+	context.fillRect(50, 50, 1, 1); // RECTANGLE FILL
+	cells = createCells(); // CREATE CELLS
 }
 
-// RANDOM FUNCTION
+// CREATE CELLS FUNCTION
+function createCells() {
+	let arrRows = new Array(resolution);
+	for (let x = 0; x < resolution; x++) {
+		let cols = new Array(resolution);
+		for (let y = 0; y < resolution; y++) {
+			cols[y] = false;
+		}
+		arrRows[x] = cols;
+	}
+	return arrRows;
+}
+
+// RANDOM CELLS FUNCTION
 function randomCells() {
 	for (let y = 0; y < resolution; y++) {
 		for (let x = 0; x < resolution; x++) {
 			if (Math.random() < 0.5) {
+				cells[x][y] = true;
+			}
+		}
+	}
+}
+
+// DRAW CELLS
+function drawCells() {
+	for (let y = 0; y < resolution; y++) {
+		for (let x = 0; x < resolution; x++) {
+			if (cells[x][y]) {
 				context.fillRect(x, y, 1, 1);
 			}
 		}
